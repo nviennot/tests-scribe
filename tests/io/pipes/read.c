d59c2921dc985077d4a46f6752d164a1afbee093
Nicolas Viennot <nicolas@viennot.biz>
2011-01-18 21:08:19 -0500

    IO/Pipes: Added a few tests

---
+++ tests/io/pipes/read.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <scribe.h>
+
+void sig_handler(int signum)
+{
+	printf("Got SIGPIPE\n");
+}
+
+void register_sig_handler()
+{
+	struct sigaction action;
+	action.sa_handler = sig_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	sigaction(SIGPIPE, &action, NULL);
+}
+
+void do_sleep(void *arg)
+{
+	usleep(10000);
+}
+
+int main(int argc, char **argv)
+{
+	int pipe_fds[2];
+	int rfd, wfd;
+	int res;
+	char buffer[100];
+	register_sig_handler();
+
+	pipe(pipe_fds);
+	rfd = pipe_fds[0];
+	wfd = pipe_fds[1];
+
+	if (!fork()) {
+		close(rfd);
+		write(wfd, "Hello ", strlen("Hello "));
+		scribe_on_replay(do_sleep, NULL);
+		write(wfd, "World", strlen("World"));
+		return 0;
+	}
+	close(wfd);
+
+	usleep(1000);
+
+	while (1) {
+		res = read(rfd, buffer, sizeof(buffer));
+		if (res > 0) {
+			buffer[res] = 0;
+			printf("Read %d bytes: %s\n", res, buffer);
+		} else if (res == 0) {
+			printf("EOF\n");
+			break;
+		} else {
+			perror("read failed\n");
+			break;
+		}
+	}
+
+	return 0;
+}
---
+++ tests/io/pipes/write.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <scribe.h>
+
+void sig_handler(int signum)
+{
+	printf("Got SIGPIPE\n");
+}
+
+void register_sig_handler()
+{
+	struct sigaction action;
+	action.sa_handler = sig_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	sigaction(SIGPIPE, &action, NULL);
+}
+
+void do_sleep(void *arg)
+{
+	usleep(10000);
+}
+
+int main(int argc, char **argv)
+{
+	int pipe_fds[2];
+	int rfd, wfd;
+	int res;
+	char buffer[100];
+	register_sig_handler();
+
+	pipe(pipe_fds);
+	rfd = pipe_fds[0];
+	wfd = pipe_fds[1];
+
+	if (!fork()) {
+		close(rfd);
+		scribe_on_replay(do_sleep, NULL);
+		res = write(wfd, "Hello ", strlen("Hello "));
+		printf("write() = %d\n", res);
+		write(wfd, "World", strlen("World"));
+		printf("write() = %d\n", res);
+		return 0;
+	}
+	close(wfd);
+	usleep(1000);
+	close(rfd);
+	return 0;
+}
