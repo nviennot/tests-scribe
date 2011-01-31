affbcc44ebe874819a28c880fe33caaab4686101
Nicolas Viennot <nicolas@viennot.biz>
2011-01-31 03:20:42 -0500

    IO/Read: Added a read /dev/zero test

---
+++ tests/io/read/dev_zero.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	char buffer[100];
+	int frand = open("/dev/zero", O_RDONLY);
+	read(frand, buffer, sizeof(buffer));
+	write(1, buffer, sizeof(buffer));
+	return 0;
+}
