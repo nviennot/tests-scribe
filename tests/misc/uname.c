2fe87f79e02f5c08f9b0bb57f7cfc580bf694701
Nicolas Viennot <nicolas@viennot.biz>
2011-03-07 07:04:40 -0500

    Misc/Uname: Code refactored to use the macro scribe_on_replay()

--- tests/misc/uname.c
+++ tests/misc/uname.c
@@ -5,41 +5,42 @@
 #include <fcntl.h>
 #include <sys/utsname.h>
 
+#define BUFFER_SIZE 4096
+
+void on_pre_replay(void *arg)
+{
+	char new_hostname[BUFFER_SIZE];
+	char *old_hostname = arg;
+	int rand;
+	int frand;
+
+	/*
+	 * We want to change the hostname, to change the real uname to
+	 * validate the data copy.
+	 */
+	gethostname(old_hostname, BUFFER_SIZE);
+
+	frand = open("/dev/urandom", O_RDONLY);
+	read(frand, &rand, sizeof(rand));
+	close(frand);
+	sprintf(new_hostname, "scribe_test_hostname_%08X", rand);
+	sethostname(new_hostname, strlen(new_hostname));
+}
+
+void on_post_replay(void *arg)
+{
+	char *old_hostname = arg;
+	sethostname(old_hostname, strlen(old_hostname));
+}
+
 int main(int argc, char **argv)
 {
-	char new_hostname[4096];
-	char old_hostname[4096];
+	char old_hostname[BUFFER_SIZE];
 	struct utsname uts;
 
-	if (scribe_is_replaying()) {
-		int rand;
-		int frand;
-
-		/*
-		 * We want to change the hostname, to change the real uname to
-		 * validate the data copy.
-		 */
-		scribe_disable();
-
-		gethostname(old_hostname, sizeof(old_hostname));
-
-		frand = open("/dev/urandom", O_RDONLY);
-		read(frand, &rand, sizeof(rand));
-		close(frand);
-		sprintf(new_hostname, "scribe_test_hostname_%08X", rand);
-		sethostname(new_hostname, strlen(new_hostname));
-		scribe_enable();
-	}
-	clear_regs();
-
+	scribe_on_replay(on_pre_replay, old_hostname);
 	uname(&uts);
-
-	if (scribe_is_replaying()) {
-		scribe_disable();
-		sethostname(old_hostname, strlen(old_hostname));
-		scribe_enable();
-	}
-	clear_regs();
+	scribe_on_replay(on_post_replay, old_hostname);
 
 	write(1, &uts, sizeof(uts));
 
